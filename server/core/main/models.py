dfbf5163cad67bdf244b3a82b19b21ef3fda4e07
Stanislav Mosin <104845230+MosinPRSS@users.noreply.github.com>
2025-03-02 13:12:43 +0300

    models

--- server/core/main/models.py
+++ server/core/main/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class users(models.Model):
+    username = models.CharField(max_length=40, unique=True)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+    auth = models.ForeignKey('auth', on_delete=models.CASCADE)
+    
+
+class sessions(models.Model):
+    session_id = models.TextField() # в sha-256 или это приватный ключ RSA
+
+class bots(models.Model):
+    ...
+
+class auth(models.Model):
+    ...
+
+class personas(models.Model):
+    ...
