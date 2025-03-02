ffb89d9a31a501a25192f64b1070ede75065e1ec
Stanislav Mosin <104845230+MosinPRSS@users.noreply.github.com>
2025-03-02 23:50:57 +0300

    cont. building

--- server/core/main/models.py
+++ server/core/main/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+import os
+
+def user_directory_path(instance, filename): # по идее ЭТО решение?
+    return 'user_{0}/{1}'.format(instance.user.id, filename)
 
 class users(models.Model):
     username = models.CharField(max_length=40, unique=True)
@@ -9,16 +13,25 @@ class users(models.Model):
     
 
 class sessions(models.Model):
-    session_id = models.TextField() # в sha-256 или это приватный ключ RSA
+    # token = models.TextField() # в sha-256 или это приватный ключ RSA
+    # username = models.ForeignKey() # сослаться на users
+    ...
 
 class bots(models.Model):
-    ...
+    botname = models.CharField(max_length=40, unique=True)
+    description = models.TextField(max_length=2000)
+    public_desc = models.TextField(max_length=2000) 
+    scenario = models.TextField(max_length=2000)
+    
+
 
 class auth(models.Model):
     email = models.EmailField(max_length=254, unique=True)
+    updated_at = models.DateField(auto_now=True)
     password = models.TextField()
 
 class user_files(models.Model):
-    avatar = models.ImageField('')
-    personas = models.FileField('')
+    avatar = models.ImageField(upload_to=user_directory_path) # подумоть
+    personas = models.FileField(upload_to=user_directory_path) # подумоть
+    chats = models.FileField(upload_to=user_directory_path) # подумоть    
     
