351a9eb888aa8b01a3ac8d5755b25771f353fc05
Stanislav Mosin <104845230+MosinPRSS@users.noreply.github.com>
2025-03-05 11:57:24 +0300

    models integration

--- server/core/main/models.py
+++ server/core/main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-    
+user_folder = User.get_username()
+
+class user_files(models.Model):
+    user_description = models.TextField(max_length=2048)
+    avatar = models.ImageField(upload_to=f"{user_folder}/")
+    chats = models.FileField()
+    personas = models.FileField()
+    user_data = models.ForeignKey(to="User")
+
+class bots(models.Model):
+    botname = models.TextField(max_length=50)
+    public_description = models.TextField(max_length=2048)
+    description = models.TextField(max_length=8192) # for prompt generation
+    avatar = models.ImageField(upload_to=f"{botname}/")
+    chat_count = models.IntegerField()
