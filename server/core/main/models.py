0484fad1c543d5273c134d0d0fee667032f84e53
Stanislav Mosin <104845230+MosinPRSS@users.noreply.github.com>
2025-03-08 18:23:06 +0300

    added a models

--- server/core/main/models.py
+++ server/core/main/models.py
@@ -1,17 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-user_folder = User.get_username()
 
 class user_files(models.Model):
     user_description = models.TextField(max_length=2048)
-    avatar = models.ImageField(upload_to=f"{user_folder}/")
-    chats = models.FileField()
-    personas = models.FileField()
-    user_data = models.ForeignKey(to="User")
+    avatar = models.ImageField(upload_to=f"avatars/") # по юзернейму
+    chats = models.FileField(upload_to=f"chats/")
+    personas = models.FileField(upload_to=f"personas/")
+    user_data = models.ForeignKey(to="User", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return
 
 class bots(models.Model):
-    botname = models.TextField(max_length=50)
+    botname = models.TextField(max_length=256)
     public_description = models.TextField(max_length=2048)
     description = models.TextField(max_length=8192) # for prompt generation
     avatar = models.ImageField(upload_to=f"{botname}/")
