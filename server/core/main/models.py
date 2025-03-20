8a695f88a37cf5791c99f4c16681155ca320f581
Stanislav Mosin <104845230+MosinPRSS@users.noreply.github.com>
2025-03-20 22:55:22 +0300

    User Creating

--- server/core/main/models.py
+++ server/core/main/models.py
@@ -4,7 +4,8 @@ from django.contrib.auth.models import User
 
 class UserData(models.Model):
     account = models.OneToOneField(User, on_delete=models.CASCADE)
-    avatar = models.ImageField(upload_to="avatars/users", verbose_name=User.username)
+    description = models.TextField(max_length=2048)
+    user_image = models.ImageField(upload_to="avatars/users/")
     
 
 
@@ -12,10 +13,14 @@ class Personas(models.Model):
     username = models.ForeignKey(to=UserData, on_delete=models.CASCADE)
     name = models.CharField(max_length=128)
     description = models.TextField()
+    persona_avatar = models.ImageField(upload_to="avatars/personas/")
 
 
 class BotData(models.Model):
     botname = models.CharField(max_length=128)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+
     pub_desc = models.TextField(max_length=2048)
     description = models.TextField(max_length=8192)
+    is_public = models.BooleanField()
     
--- server/core/main/serializers.py
+++ server/core/main/serializers.py
@@ -1,2 +1,12 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "password"]
+        extra_kwargs = {"password": {"write_only": True}}
+    def create(self, valid_data):
+        print(valid_data)
+        user = User.objects.create_user(**valid_data)
+        return user
