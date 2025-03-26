88777ac15641969c8dc17dca29c46b54d759cbd1
Stanislav Mosin <104845230+MosinPRSS@users.noreply.github.com>
2025-03-26 12:32:42 +0300

    аутенфикация по почте

--- server/core/main/models.py
+++ server/core/main/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
-
 class UserData(models.Model):
     account = models.OneToOneField(User, on_delete=models.CASCADE)
     description = models.TextField(max_length=2048)
