f1296491f5306cdaf4c3c6e49a2eb4ea5205423d
Stanislav Mosin <ssav@sfedu.ru>
2025-04-14 15:59:00 +0300

    re-work ai module

--- server/core/main/models.py
+++ server/core/main/models.py
@@ -4,35 +4,16 @@ from django.utils import timezone
 
 class UserData(models.Model):
     account = models.OneToOneField(User, on_delete=models.CASCADE)
-    description = models.TextField(max_length=2048)
-    user_image = models.ImageField(upload_to="avatars/users/")
+    avatar = models.ImageField(to="/img/users")
 
 class Personas(models.Model):
-    username = models.ForeignKey(to=UserData, on_delete=models.CASCADE)
+    user = models.ForeignKey(to=UserData, on_delete=models.CASCADE)
     name = models.CharField(max_length=128)
     description = models.TextField(max_length=1024)
     persona_avatar = models.ImageField(upload_to="avatars/personas/")
 
-
-class BotData(models.Model):
-    botname = models.CharField(max_length=128, unique=True)
-    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
-    bot_avatar = models.ImageField(upload_to="avatars/bots/")
-
-    pub_desc = models.TextField(max_length=2048)
-    description = models.TextField(max_length=8192)
-    scenario = models.TextField(max_length=4096, default=" ")
-    first_message = models.TextField(max_length=2048, default=" ")
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    modified_at = models.DateTimeField(auto_now=True)
-    is_public = models.BooleanField()
-    rating = models.IntegerField(default=0)
-
 # re-work chat models
 class ChatsData(models.Model):
-    role = models.TextField()
-    sent_time = models.DateTimeField(auto_now=True)
-    chat_number = models.IntegerField()
+    ...
 
     
