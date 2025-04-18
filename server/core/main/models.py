44574e33ba41727c1ffa9462afb924bbad7ba8da
Stanislav Mosin <ssav@sfedu.ru>
2025-04-18 18:14:48 +0300

    Recreated (100%) models, API Upgrade... (and tested)

--- server/core/main/models.py
+++ server/core/main/models.py
@@ -2,18 +2,32 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
-class UserData(models.Model):
-    account = models.OneToOneField(User, on_delete=models.CASCADE)
-    avatar = models.ImageField(to="/img/users")
+class User_Extended(models.Model):
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to="img/user/")
+    description = models.TextField(max_length=1024)
+
+class Chatbots(models.Model):
+    belongs_to = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=128, unique=True)
+    avatar = models.ImageField(upload_to="img/bot/")
+    
+    description = models.TextField(max_length=16384)
+    scenario = models.TextField(max_length=8192)
+    first_message = models.TextField(max_length=1024)
+
+    rate = models.IntegerField(default=0)
+    is_public = models.BooleanField(default=False)
+    public_description = models.TextField() # no generation
 
 class Personas(models.Model):
-    user = models.ForeignKey(to=UserData, on_delete=models.CASCADE)
+    belongs_to = models.ForeignKey(to=User, on_delete=models.CASCADE)
     name = models.CharField(max_length=128)
-    description = models.TextField(max_length=1024)
-    persona_avatar = models.ImageField(upload_to="avatars/personas/")
+    avatar = models.ImageField(upload_to="img/personas/")
+    description = models.TextField(max_length=8192)
 
-# re-work chat models
-class ChatsData(models.Model):
-    ...
-
-    
+class Sessions(models.Model):
+    belongs_to = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    conversation_code = models.CharField(max_length=64)
+    chatbot = models.ForeignKey(to=Chatbots, on_delete=models.CASCADE)
+    conversation = models.JSONField(null=True)
