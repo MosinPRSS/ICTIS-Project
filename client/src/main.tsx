24b70b3707c7fe5724df05252e20b6caa4a786e2
Stanislav Mosin <ssav@sfedu.ru>
2025-04-20 21:37:19 +0300

    continue frontend (routes n thoughts)

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from "react-router";
 import './index.css'
-import App from './App.tsx'
+import MainPage from './pages/main';
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Router>
+      <Routes>
+        <Route path='/' element={<MainPage />}></Route>
+      </Routes>
+    </Router>
+  </StrictMode>
+);
