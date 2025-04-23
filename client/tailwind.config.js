f06843bf1c98abb31e96d2ed7219c6bbcfdb6060
Stanislav Mosin <ssav@sfedu.ru>
2025-04-23 12:14:06 +0300

    example of custom colors config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,1 +1,17 @@
 // TODO: Custom Colors maybe?
+/* example
+
+theme: {
+    colors: {
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
+
+*/
