94632e772f8c675ee24979711369f4b26727e90a
Stanislav Mosin <ssav@sfedu.ru>
2025-04-23 12:17:47 +0300

    continue conf

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,17 +1,12 @@
 // TODO: Custom Colors maybe?
-/* example
+// example
+/** @type {import('tailwindcss').Config} */
 
-theme: {
-    colors: {
-      'blue': '#1fb6ff',
-      'purple': '#7e5bef',
-      'pink': '#ff49db',
-      'orange': '#ff7849',
-      'green': '#13ce66',
-      'yellow': '#ffc82c',
-      'gray-dark': '#273444',
-      'gray': '#8492a6',
-      'gray-light': '#d3dce6',
-    },
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
 
-*/
