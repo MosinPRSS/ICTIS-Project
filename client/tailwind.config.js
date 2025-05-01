1f7f2a02f269116b45ed85ee8010c03022086f5d
Qua11ra <veperelygin930@gmail.com>
2025-05-01 14:06:27 +0300

    попытка 1
    я его рот манал

---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+// TODO: Custom Colors maybe?
+// example
+/** @type {import('tailwindcss').Config} */
+
+export default {
+    content: [],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
